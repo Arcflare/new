651fdfd38c4f59595a217dad384cd2077e556d78
Arcflare <pro@live.ru>
2024-11-03 16:33:09 +0300

    1

--- public/script.js
+++ public/script.js
@@ -1,8 +1,4 @@
-// api/send.js
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        res.status(200).json({ success: true, message: "Функция работает!" });
-    } else {
-        res.status(405).json({ success: false, message: "Метод не поддерживается" });
-    }
-}
+fetch("/send", {
+    method: "POST",
+    body: formData,
+});
