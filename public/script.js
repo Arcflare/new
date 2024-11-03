23567a475ac7daa4538f7efa1932bde48c65978b
Arcflare <pro@live.ru>
2024-11-03 16:19:48 +0300

    1

--- public/script.js
+++ public/script.js
@@ -1,9 +1,8 @@
 // api/send.js
-
-module.exports = async (req, res) => {
+export default function handler(req, res) {
     if (req.method === 'POST') {
         res.status(200).json({ success: true, message: "Функция работает!" });
     } else {
         res.status(405).json({ success: false, message: "Метод не поддерживается" });
     }
-};
+}
